357d45daa1e48c51e63477b471d78d818e3de04a
psdcode <patrick.dorosz@gmail.com>
2019-03-04 22:42:16 -0500

    Complete

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,50 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      emoji: 'Loading...'
+    };
+  }
+
+  componentDidMount () {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (
+      prevProps.entries === this.props.entries
+      && prevProps.name === this.props.name
+    ) {
+      return null
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://pi5ixw0f79.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(response => response.json())
+      .then(data => this.setState({ emoji: data.input }))
+      .catch(console.log)
+  };
+
+  render () {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
+        <div className='white f3'>
+          {`Rank Badge: ${this.state.emoji}`}
+        </div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Rank;
